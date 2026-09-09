3b22c7c8df5257ebf30dc110e85a377888991a58
mustafahossameldin66-dotcom <mustafahossameldin66@gmail.com>
2026-09-09 20:48:42 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="mihrab-v20-0";
-const CORE=["./","./index.html","./styles.css?v=20.0","./app.js?v=20.0","./manifest.webmanifest","./assets/mihrab-logo.svg","./assets/icon.svg","./assets/icon-192.png","./assets/icon-512.png"];
+const CACHE="mihrab-v19-0";
+const CORE=["./","./index.html","./styles.css?v=19.0","./app.js?v=19.0","./manifest.webmanifest","./assets/mihrab-logo.svg","./assets/icon.svg","./assets/icon-192.png","./assets/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.origin!==self.location.origin)return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return res;}).catch(()=>caches.match("./index.html"))))});
